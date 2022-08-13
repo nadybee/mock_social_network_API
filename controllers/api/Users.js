160232c75f8c045fc7ddfc13d06a5a0ed5341594
Nadybee <natalie@yoodlize.com>
2022-08-13 12:33:37 -0600

    added gitignore

--- controllers/api/Users.js
+++ controllers/api/Users.js
@@ -1,3 +1,28 @@
 const router = require("express").Router()
+const db = require('../../server')
+
+
+router.get('/', (req, res) => {
+    db.collection('users')
+      .find({ featured: true })
+      .toArray((err, results) => {
+        if (err) throw err;
+        res.send(results);
+      });
+  });
+  
+
+// app.post('/create', (req, res) => {
+//     // The title and author will be provided by the request body
+//     db.collection('bookCollection').insertOne(
+//       { title: req.body.title, author: req.body.author },
+//       (err, results) => {
+//         if (err) throw err;
+//         res.json(results);
+//       }
+//     );
+//   });
+
+
 
 module.exports =router
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const dotenv = require('dotenv')
 const express = require('express');
 const routes = require('./controllers');
+const mongodb = require('mongodb').MongoClient;
+
+dotenv.config({path})
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
+const connectionStringURI = `mongodb://127.0.0.1:27017/social_mock`;
+
+let db;
+
+mongodb.connect(
+    connectionStringURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      db = client.db();
+      app.listen(PORT, () => {
+        console.log(`Example app listening at http://localhost:${PORT}`);
+      });
+    }
+  );
+  
+
+  
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => console.log(`now listening on port ${PORT}`))
+module.exports = {db}
+
